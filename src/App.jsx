b0c5448379752cc21c8d49e0ab2f44ea839fd4bf
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-10 12:54:09 +0100

    add the ather two array element for to create trhee article

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,25 @@ function App() {
       contenuto:
         "La transizione verso i veicoli elettrici sta cambiando radicalmente il settore dei trasporti. Tuttavia, rimangono sfide significative legate alla produzione e allo smaltimento delle batterie."
     },
-
-  ]
+    {
+      id: 2,
+      titolo: "Energia rinnovabile: il futuro dell'Europa",
+      autore: "Giulia Bianchi",
+      data: "9 Novembre 2025",
+      immagine: "https://generplus.it/wp-content/uploads/2021/06/energie-rinnovabili-europa.jpg",
+      contenuto:
+        "Le politiche europee stanno accelerando l’adozione di fonti rinnovabili. L’obiettivo è ridurre la dipendenza dai combustibili fossili e abbattere le emissioni di CO₂ entro il 2040."
+    },
+    {
+      id: 3,
+      titolo: "Batterie al litio e impatto ambientale",
+      autore: "Luca Verdi",
+      data: "8 Novembre 2025",
+      immagine: "https://www.large-battery.com/wp-content/uploads/2025/06/5b2a4645902e4c40a1ac485e0a8b0712.jpg",
+      contenuto:
+        "L’estrazione del litio è un tema cruciale per comprendere la sostenibilità della mobilità elettrica. Nuove tecnologie promettono di ridurne l’impatto ambientale."
+    }
+  ];
 
   return (
     <>
