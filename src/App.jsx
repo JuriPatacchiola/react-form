b91d173c8459d20bdbd6e7633d972fea14502fc4
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-10 12:53:19 +0100

    add the first array element

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 
 
 function App() {
+  const articoli = [
+    {
+      id: 1,
+      titolo: "La transizione ai veicoli elettrici",
+      autore: "Mario Rossi",
+      data: "10 Novembre 2025",
+      immagine: "https://www.motorionline.com/wp-content/uploads/2024/03/classifica-auto-inquinanti-2024-1024x682.jpg",
+      contenuto:
+        "La transizione verso i veicoli elettrici sta cambiando radicalmente il settore dei trasporti. Tuttavia, rimangono sfide significative legate alla produzione e allo smaltimento delle batterie."
+    },
 
+  ]
 
   return (
     <>
